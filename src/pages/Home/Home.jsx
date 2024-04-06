097c7413503d0d1ea5d2d0febd32c8e2fda6db0d
Shafayathub <shafayat.ms@gamilcom>
2024-04-07 01:15:06 +0600

    service card added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import Header from "../Shared/Header";
 import Banner from "./components/Banner";
-
+import ServiceCard from "./components/serviceCard";
 
 export default function Home() {
   return (
-    <div>
-          <Header/>
-        <Banner/>
+    <div className="p-10">
+      <Header />
+      <Banner />
+      <ServiceCard />
     </div>
-  )
+  );
 }
