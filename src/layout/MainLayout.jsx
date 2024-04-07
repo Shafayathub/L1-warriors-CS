72f3265d9c43a06431c89f845c4179af6acbc6a7
mehedi-imun <mehediimun@gmail.com>
2024-04-07 20:53:35 +0600

    add darkmode

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,18 @@
-import Header from "../pages/Shared/Header";
+import { useState } from "react";
+import { ThemeContext } from "../context";
 import Home from "../pages/Home/Home";
+import Header from "../pages/Shared/Header";
 
 const MainLayout = () => {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div>
+    <div className={`${darkMode ? "dark":""}`}>
+      <ThemeContext.Provider  value={{ darkMode, setDarkMode }}>
       <Header />
-      <Home></Home>
+
+<Home></Home>
+      </ThemeContext.Provider>
+    
     </div>
   );
 };
